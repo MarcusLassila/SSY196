5be9bc4a843a59dcaf8c528c4a780862344d0778
Marcus Lassila <marcus@ducce.net>
2024-01-29 00:33:53 +0100

    starting homework 1

---
+++ hw1/R19.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+
+
+def to_dB(x):
+    return 10 * np.log10(x)
+
+def from_dB(x):
+    return 10 ** (x / 10)
+
+def bin_entropy(x):
+    return x * np.log2(1 / x) + (1 - x) * np.log2(1 / (1 - x))
+
+def inv_bin_entropy(x):
+    '''
+    Inverse binary entropy function computed by binary search.
+    Return value in range [0, 0.5].
+    '''
+    eps = 1e-6
+    low, high = 0, 0.5
+    while low + eps < high:
+        mid = (low + high) / 2
+        entr = bin_entropy(mid)
+        if abs(entr - x) < eps:
+            break
+        if entr < x:
+            low = mid
+        else:
+            high = mid
+    return mid
+
+class BI_AWGN:
+
+    def __init__(self, noise_std):
+        self.noise_std = noise_std
+
+    def __call__(self):
+        loc = 1.0 if np.random.random() < 0.5 else -1.0
+        return np.random.normal(loc=loc, scale=self.noise_std)
+
+    def density(self, x):
+        scale = 1 / np.sqrt(2 * np.pi) / self.noise_std
+        pos = scale * np.exp(-(x - 1) ** 2 / (2 * self.noise_std ** 2))
+        neg = scale * np.exp(-(x + 1) ** 2 / (2 * self.noise_std ** 2))
+        return 0.5 * (pos + neg)
+
+    def capacity(self, num_samples=int(2e6)):
+        monte_carlo = -sum(np.log2(self.density(self.__call__())) for _ in range(num_samples)) / num_samples
+        guassian_entropy = -0.5 * np.log2(2 * np.pi * np.e * self.noise_std ** 2)
+        return monte_carlo + guassian_entropy
+
+
+rate = 0.5
+
+def error_prob(snr_dB):
+    noise_std = (2 * rate * from_dB(snr_dB)) ** -0.5
+    bin_entr = 1 - BI_AWGN(noise_std).capacity() / rate
+    return inv_bin_entropy(bin_entr)
+
+snrs = np.linspace(-0.4, 0.185)
+pbs  = [*map(error_prob, tqdm(snrs, desc=f"Computing figure 1.8 with rate={rate}"))]
+
+plt.plot(snrs, pbs)
+plt.yscale('log')
+plt.xlabel("E/N")
+plt.ylabel("pb")
+plt.show()
