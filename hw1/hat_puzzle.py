7218f4a1c716dde614ab234b0d64fc71882de987
Marcus Lassila <marcus@ducce.net>
2024-01-30 14:39:57 +0100

    more hat puzzle

--- hw1/hat_puzzle.py
+++ hw1/hat_puzzle.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import comb, log2
 
 def strategy(hats):
     guesses = []
@@ -31,6 +32,45 @@ def simulate_game(n = 3):
             break
     return score > 0
 
-num_games = 1000000
-win_precentage = sum(simulate_game(3) for _ in range(num_games)) / num_games
-print(win_precentage)
+# num_games = 1000000
+# win_precentage = sum(simulate_game(7) for _ in range(num_games)) / num_games
+# print(win_precentage)
+
+# 2-majority
+# r b b  =>  6 correct
+# b b b  =>  2 wrong
+
+# 2-majority
+# r r r b b b b  =>  70 correct
+# r r b b b b b  =>  42 wrong
+# r b b b b b b  =>  14 wrong
+# b b b b b b b  =>   2 wrong
+
+# 4-majority
+# r r r b b b b  =>  35 correct avg
+# r r b b b b b  =>  42 correct
+# r b b b b b b  =>  14 wrong
+# b b b b b b b  =>   2 wrong
+
+# 6-majority
+# r r r b b b b  =>  35 correct avg
+# r r b b b b b  =>  21 correct avg
+# r b b b b b b  =>  14 correct
+# b b b b b b b  =>   2 wrong
+
+def find_best(n):
+    assert log2(n + 1) % 1 == 0
+    scores = []
+    for b in range(n // 2, 0, -1):
+        score = 0
+        for k in range(n // 2, -1, -1):
+            c = comb(n, k)
+            if k > b:
+                score += c
+            elif k == b:
+                score += 2 * c
+        score /= 2 ** n
+        scores.append(score)
+    return max(scores)
+
+print("best win percentage:", find_best(15))
